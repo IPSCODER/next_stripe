9b37d1281e4a1fafc604de22dc960d1cbe61a4b6
ipscoder <psawant0015@gmail.com>
2024-06-26 22:54:02 +0530

    A

--- src/components/stripe/Payment.tsx
+++ src/components/stripe/Payment.tsx
@@ -5,6 +5,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckOutFrom";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
+import stripe from "stripe";
+
+const stripeInstance = new stripe("sk_test_51Nnz1ASDTdclJtfgBhdXYXT5FiSJcqVu4sMJyFt3olCg6AYVUdFIzLLkOefULbw5XKeFtOVQXC6ziWsdGyLfUWsT00VWW2utEc");
 
 function Payment() {
   const [stripePromise, setStripePromise] = useState<any>(null);
@@ -17,8 +20,16 @@ function Payment() {
 
 
   const payment_intent = async () =>{
-    const next_resullt = await axios.post("/api/stripe_key");
-    setClientSecret(next_resullt.data.clientSecret);
+    const paymentIntent:any = await stripeInstance.paymentIntents.create({
+        amount: 1999, // Amount in cents
+        currency: "eur", // Currency must be in lowercase
+        // payment_method_types: ["card"], // Specify payment method types
+      });
+      setClientSecret(paymentIntent.client_secret);
+
+
+    //   const next_resullt = await axios.post("/api/stripe_key");
+    //   setClientSecret(next_resullt.data.clientSecret);
   }
 
 
